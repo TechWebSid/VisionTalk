23329cc13626597e9a71158ad9a113fc789efef9
TechWebSid <siddharthasrivastava30@gmail.com>
2024-04-05 21:04:42 +0530

    first commit

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,15 +2,13 @@ import MeetingTypeList from '@/components/MeetingTypeList';
 
 const Home = () => {
   const now = new Date();
-
-  const time = now.toLocaleTimeString('en-IN', { hour: '2-digit', minute: '2-digit' });
-  const date = (new Intl.DateTimeFormat('en-IN', { dateStyle: 'full' })).format(now);
-
+  const time = now.toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit' });
+  const date = (new Intl.DateTimeFormat(undefined, { dateStyle: 'full' })).format(now);
   return (
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
+          <h2 className=" max-w-[273px] rounded py-2 text-center text-base font-normal">
           Connect effortlessly with VisionTalk
           </h2>
           <div className="flex flex-col gap-2">
